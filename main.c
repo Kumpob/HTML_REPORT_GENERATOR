81016c4a73334a3ca58e4c9c95e87d284662ac5a
Kumpob <90686608+Kumpob@users.noreply.github.com>
2021-12-05 02:46:47 +0700

    Update main.c

--- main.c
+++ main.c
@@ -37,10 +37,12 @@ int main()
             if(t%3==1){
                 strcpy(no[t2].fn,s);
                 remove_spaces(no[t2].fn);
+                no[t2].fn[strcspn(no[t2].fn, "\r\n")] = 0;
             }
             else if(t%3==2){
                 strcpy(no[t2].ln,s);
                 remove_spaces(no[t2].ln);
+                no[t2].ln[strcspn(no[t2].ln, "\r\n")] = 0;
             }
             else if(t%3==0){
                 sscanf(s, "%d", &no[t2].sn);
@@ -61,16 +63,16 @@ int main()
     fprintf(output,"<head>\n");
     fprintf(output,"<title>Html report generator</title>\n");
     fprintf(output,"<style>\n");
-    fprintf(output,"table, td, th{border: 1px solid black}\n");
+    fprintf(output,"\n");
     fprintf(output,"</style>\n");
     fprintf(output,"</head>\n");
     fprintf(output,"<body>\n");
     fprintf(output,"<h1>\n");
     fprintf(output,"<center>\n");
     fprintf(output,"Html report generator\n");
-    fprintf(output,"Employee List\n");
-    fprintf(output,"</center>\n");
-    fprintf(output,"<table >\n");
+    fprintf(output,"<br>Employee List\n");
+    fprintf(output,"</h1>\n");
+    fprintf(output,"<table border=\"1\", width=\"800\" style=\"margin-left:auto;margin-right:auto;\">\n");
     fprintf(output,"<tr>\n");
     fprintf(output,"<th>Serial Number</th>\n");
     fprintf(output,"<th>First Name</th>\n");
@@ -84,7 +86,7 @@ int main()
         fprintf(output,"</tr>\n");
     }
     fprintf(output,"</table>\n");
-    fprintf(output,"</h1>\n");
+    fprintf(output,"</center>\n");
     fprintf(output,"</body>\n");
     fprintf(output,"</html>\n");
     return 0;
