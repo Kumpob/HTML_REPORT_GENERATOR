922d126e8327be9df13690f693f129ee390ff224
Kumpob <90686608+Kumpob@users.noreply.github.com>
2021-12-16 22:33:46 +0700

    split code to functions

--- newmain2.c
+++ newmain2.c
@@ -8,6 +8,7 @@ struct emp{
     char ln[20];
     char email[20];
 };
+
 void remove_spaces(char* s) {
     char* d = s;
     do {
@@ -16,16 +17,14 @@ void remove_spaces(char* s) {
         }
     } while (*s++ = *d++);
 }
-int main()
-{
-    char fi[20];
+
+char titletaker(char *title){
     char tf[20];
     char tu[25];
     printf("Enter title file(without .txt) name: ");
     gets(tf);
     tf[strcspn(tf, "\r\n")] = 0;
     sprintf(tu,"%s.txt",tf);
-    char title[50];
     FILE *fp2=fopen(tu,"r");
     const size_t line_size = 300;
     char* line = malloc(line_size);
@@ -34,58 +33,34 @@ int main()
         }
     free(line);
     fclose(fp2);
-    printf("enter your file(without .txt) name: ");
-    gets(fi);
-    char fu[25];
-    sprintf(fu,"%s.txt",fi);
-    FILE *fp=fopen(fu,"r");
-    int tl;
-    char tmp[50] = {0};
-    fgets(tmp, 50, fp);
-    tl = atoi(tmp);
-    struct emp no[tl];
-    char line2[80] = {0};
-    unsigned int line_count = 0;
-    unsigned int line_count2 = 0;
-    int l;
-    int nl=0;
-    while (fgets(line2, 80, fp))
-    {
-        ++line_count;
-        if((line_count)%5 == 1)
-            strcpy(no[line_count2].fn, line2);
-        else if((line_count)%5 == 2)
-            strcpy(no[line_count2].ln, line2);
-        else if((line_count)%5 == 3)
-            sscanf(line2, "%d", &no[line_count2].sn);
-        else if((line_count)%5 == 4)
-            strcpy(no[line_count2].email, line2);
-        else if((line_count)%5 == 0){
-            char a[100];
-            int i=0,n=0;
-            double d[5];
-            char temp[100];
-            int l2=0;
-            strcpy(temp,line2);
-            while(temp[i]!='\n'){
-                if(temp[i]!=' '){
-                    a[n]=temp[i];
-                    n++;
-                }
-                else{
-                    sscanf(a, "%lf", &no[line_count2].GPA[l2]);
-                    strcpy(a,"");
-                    n=0;
-                    l2=l2+1;
-                    nl=nl+1;
-                }
-                i++;
-            }
-            ++line_count2;
-            }
-        l=line_count2;
+    return title;
+}
+
+int gpataker(char *line2, int line_count2, struct emp *no, int nl){
+    char a[100];
+    int i=0,n=0;
+    double d[5];
+    char temp[100];
+    int l2=0;
+    strcpy(temp,line2);
+    while(temp[i]!='\n'){
+        if(temp[i]!=' '){
+            a[n]=temp[i];
+            n++;
+        }
+        else{
+            sscanf(a, "%lf", &no[line_count2].GPA[l2]);
+            strcpy(a,"");
+            n=0;
+            l2=l2+1;
+            nl=nl+1;
+        }
+        i++;
     }
-    int a[l];
+    return nl;
+}
+
+void htmlmaker(struct emp *no,int l,int *a,int nl,char *title){
     for(int i=0;i<l;i++){
             a[i]=0;
         for(int j=0;j<5;j++){
@@ -136,5 +111,46 @@ int main()
     fprintf(out,"</body>\n");
     fprintf(out,"</html>\n");
     fclose(out);
+}
+
+int main()
+{
+    char title[50];
+    titletaker(&title);
+    char fi[20];
+    printf("enter your data file(without .txt) name: ");
+    gets(fi);
+    char fu[25];
+    sprintf(fu,"%s.txt",fi);
+    FILE *fp=fopen(fu,"r");
+    int tl;
+    char tmp[50] = {0};
+    fgets(tmp, 50, fp);
+    tl = atoi(tmp);
+    struct emp no[tl];
+    char line2[80] = {0};
+    unsigned int line_count = 0;
+    unsigned int line_count2 = 0;
+    int l;
+    int nl=0;
+    while (fgets(line2, 80, fp))
+    {
+        ++line_count;
+        if((line_count)%5 == 1)
+            strcpy(no[line_count2].fn, line2);
+        else if((line_count)%5 == 2)
+            strcpy(no[line_count2].ln, line2);
+        else if((line_count)%5 == 3)
+            sscanf(line2, "%d", &no[line_count2].sn);
+        else if((line_count)%5 == 4)
+            strcpy(no[line_count2].email, line2);
+        else if((line_count)%5 == 0){
+            nl=gpataker(&line2,line_count2,&no,nl);
+            ++line_count2;
+            }
+        l=line_count2;
+    }
+    int a[l];
+    htmlmaker(&no,l,a,nl,&title);
     return 0;
 }
